b70909042833ea52de243d007a1e0209fd10f8ce
unknown <smietnikzdupy@gmail.com>
2020-02-16 23:01:41 +0100

    to deploy v2

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.post("/failure", function(req, res){
   res.redirect("/");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server jest na porcie 3000");
 });
 
